840c679d4726df5e2591a21a62dc965cb077284c
Asif Shaukat <asif.shaukat50@gmail.com>
2024-07-11 06:50:53 -0700

    Added variable type in addition.py

--- addition.py
+++ addition.py
@@ -1,7 +1,7 @@
 # Addition the two numbers
-val1=23
-val2=34
-result=val1+val2
+val1: int = 23
+val2: int = 34
+result: int = val1 + val2
 print("result is " + str(result))
 # pushing 2nd time on github repo
 # made changes from hananali0311
